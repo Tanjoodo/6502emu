fc3a85f3c2618b898ed6e1bab8b82b07506938a6
naelbasheer <nael.albasheer@outlook.com>
2015-12-26 15:44:30 +0200

    implemented the procTXA

--- 6502emu/instruction_procedures.cpp
+++ 6502emu/instruction_procedures.cpp
@@ -472,8 +472,10 @@ void ProcTSX(AddressingMode addressingMode, uint8_t operands[])
 
 void ProcTXA(AddressingMode addressingMode, uint8_t operands[])
 {
-	cout << "TXA ";
-	PrintOperands(addressingMode, operands);
+	uint8_t& operand = _fetch_operand(addressingMode, operands);
+	reg::Accumulator = reg::X & operand;
+	SetFlagZ(reg::Accumulator == 0);
+	SetFlagN((reg::Accumulator & (1 << 7)) != 0);
 }
 
 void ProcTXS(AddressingMode addressingMode, uint8_t operands[])
